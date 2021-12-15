738b8e3509c0f53091b1cd17ddf030ecd4886b4a
Jiang Han <jianghan99@gmail.com>
2021-12-15 22:50:02 +0700

    Rename Board to BoardUtil

--- src/model/Board.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- *
- * @author Jiang Han
- */
-public class Board {
-    
-}
---
+++ src/model/BoardUtil.java
@@ -0,0 +1,49 @@
+package model;
+
+import javafx.util.Pair;
+
+/**
+ *
+ * @author Jiang Han
+ */
+public class BoardUtil {
+    /*
+    [12][13][14][15]
+    [ 8][ 9][10][11]
+    [ 4][ 5][ 6][ 7]
+    [ 0][ 1][ 2][ 3]
+    */
+    
+    public static void print(long code){
+        //TODO
+    }
+    
+    public static long encode(int[][] board){
+        //TODO
+        return 0;
+    }
+    
+    public static int[][] decode(long code) {
+        //TODO
+        return null;
+    }
+    
+    public static long generateInitialBoard() {
+        //TODO
+        return 0;
+    }
+    
+    public static Move[] getAvailableMoves(long code){
+        //TODO
+        return null;
+    }
+    public static Pair<Integer, Long> applyMove(long code, Move move) {
+        //TODO
+        return null;
+    }
+    public static int getValueAt(int pos){
+        //TODO
+        return 0;
+    }
+    
+}
