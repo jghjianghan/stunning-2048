b563a2744914b5554299b0202e21db3363dab63b
Jiang Han <jianghan99@gmail.com>
2021-12-19 23:16:26 +0700

    test BoardUtil.print() and add docs to finished methods

--- src/model/BoardUtil.java
+++ src/model/BoardUtil.java
@@ -3,7 +3,7 @@ package model;
 import javafx.util.Pair;
 
 /**
- *
+ * 
  * @author Jiang Han
  */
 public class BoardUtil {
@@ -18,19 +18,29 @@ public class BoardUtil {
     [15][14][13][12][11]...[2][1][0]
     */
     
+    /**
+     * Converts board into a printable string
+     * @param code The board in coded format
+     * @return The formatted string of the board
+     */
     public static String print(long code){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i<16; i++){
             sb.append((code & 0xf));
-            sb.append('\t');
-            code >>>= 4;
-            
             if (i%4==3)
                 sb.append('\n');
+            else
+                sb.append('\t');
+            code >>>= 4;
         }
         return sb.toString();
     }
     
+    /**
+     * Given a board as 4x4 integer array, codes the board into a long variable.
+     * @param board 4x4 integer array of the board (elements should be in range [0,15])
+     * @return The coded result of the board
+     */
     public static long encode(int[][] board){
         long code = 0;
         for(int i = 3; i>=0; i--){
@@ -42,6 +52,11 @@ public class BoardUtil {
         return code;
     }
     
+    /**
+     * Given a board data in coded format, returns it's 4x4 integer array format
+     * @param code The coded format of the board
+     * @return A 4x4 integer array representation of the board
+     */
     public static int[][] decode(long code) {
         int board[][] = new int[4][4];
         for(int i = 0; i<16; i++){
@@ -64,8 +79,14 @@ public class BoardUtil {
         //TODO
         return null;
     }
+    
+    /**
+     * Gets the value at some particular position on a board from its coded representation
+     * @param code The coded representation of the board
+     * @param pos The position in the board (0-15). 0 is top-left, 15 is bottom right
+     * @return The value of the board at some position
+     */
     public static int getValueAt(long code, int pos){
         return (int)((code & (0xfl << (pos * 4))) >>> (pos*4) );
     }
-    
 }
--- test/BoardUtilTest.java
+++ test/BoardUtilTest.java
@@ -1,3 +1,4 @@
+
 import java.util.concurrent.ThreadLocalRandom;
 import model.BoardUtil;
 import org.junit.Test;
@@ -8,53 +9,64 @@ import org.junit.Assert;
  * @author Jiang Han
  */
 public class BoardUtilTest {
+
     @Test
     public void testEncodeDecode() {
         int board[][] = new int[][]{
-            {0,0,0,0},
-            {0,0,0,0},
-            {0,0,0,0},
-            {0,0,0,0},
-        };
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},};
         int boards[][][] = new int[5][][];
         boards[0] = board;
         board = new int[][]{
-            {1,5,14,13},
-            {7,10,8,12},
-            {0,6,2,3},
-            {15,9,11,4},
-        };
+            {1, 5, 14, 13},
+            {7, 10, 8, 12},
+            {0, 6, 2, 3},
+            {15, 9, 11, 4},};
         boards[1] = board;
         ThreadLocalRandom random = ThreadLocalRandom.current();
-        for(int i = 2; i<5; i++){
+        for (int i = 2; i < 5; i++) {
             boards[i] = new int[4][4];
-            for(int j = 0; j<4; j++){
+            for (int j = 0; j < 4; j++) {
                 for (int k = 0; k < 4; k++) {
                     boards[i][j][k] = random.nextInt(16);
                 }
             }
         }
-        
-        for(int i = 0; i<boards.length; i++){
+
+        for (int i = 0; i < boards.length; i++) {
             int newBoard[][] = BoardUtil.decode(BoardUtil.encode(boards[i]));
             Assert.assertArrayEquals(boards[i], newBoard);
         }
     }
-    
+
     @Test
-    public void testGetValueAt(){
-        int board[][] = new int[4][4];;
+    public void testGetValueAt() {
+        int board[][] = new int[4][4];
         ThreadLocalRandom random = ThreadLocalRandom.current();
-        
-        for(int i = 0; i<4; i++){
+
+        for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 board[i][j] = random.nextInt(16);
             }
         }
-        
+
         long code = BoardUtil.encode(board);
-        for(int i = 0; i<16; i++){
-            Assert.assertEquals(BoardUtil.getValueAt(code, i), board[i/4][i%4]);
+        for (int i = 0; i < 16; i++) {
+            Assert.assertEquals(BoardUtil.getValueAt(code, i), board[i / 4][i % 4]);
         }
+    }
+
+    @Test
+    public void testPrint() {
+        int board[][] = new int[][]{
+            {1, 5, 14, 13},
+            {7, 10, 8, 12},
+            {0, 6, 2, 3},
+            {15, 9, 11, 4}
+        };
+        String expectedResult = "1\t5\t14\t13\n7\t10\t8\t12\n0\t6\t2\t3\n15\t9\t11\t4\n";
+        Assert.assertEquals(expectedResult, BoardUtil.print(BoardUtil.encode(board)));
     }
 }
